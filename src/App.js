fbf5f3ef73a454f2692c1b5524683a1c3abe884b
sma462 <sma462@uclive.ac.nz>
2022-07-05 14:32:09 +1200

    Page Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Page Components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
+// Styles
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route>
+            <Search path="/search" />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
